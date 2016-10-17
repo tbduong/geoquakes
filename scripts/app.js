3658f06bb75e82a47b57d3a8319675f891a927b0
tbduong <tiffaniduong@gmail.com>
2016-10-17 15:42:26 -0700

    map renders onto page

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,7 @@ var map;
 var template;
 
 $(document).on("ready", function() {
-  //Function to create the map
+  //Function called to embed map
   initMap();
 
   //AJAX call to get gmap JSON data
@@ -15,14 +15,32 @@ $(document).on("ready", function() {
   })
   //success function
   .done(function(data){
-      console.log(data);
+    // console.log(data);
+    var earthquakes = data.features;
+    console.log(data.features);
+    //need to call function that creates marker for earthquake location
+    //need to include handlebars template for quakes
   })
   //fail function
   .fail(function(response){
       console.log("Error:", response);
   });
 
+//this function will create the map
+function initMap(){
+  //define initial position on map
+  var pos = {lat: 37.77, lng: -122.45};
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: pos,
+    zoom:2
   });
+}
+
+
+
+
+
+
 
 
 });
