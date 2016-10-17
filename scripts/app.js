758d4429f896811902066483b7416840e080dbb3
tbduong <tiffaniduong@gmail.com>
2016-10-17 16:31:57 -0700

    map can render datapoints, lat and long for earthquakes

--- scripts/app.js
+++ scripts/app.js
@@ -17,7 +17,10 @@ $(document).on("ready", function() {
   .done(function(data){
     // console.log(data);
     var earthquakes = data.features;
-    console.log(data.features);
+    // console.log(earthquakes);
+    var quakeIcon = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Reddot-small.svg/400px-Reddot-small.svg.png";
+    createMarkers(earthquakes, quakeIcon);
+    compileHandlebarsTemplate(earthquakes, '#info', '#quakes-template');
   })
   //fail function
   .fail(function(response){
@@ -26,22 +29,37 @@ $(document).on("ready", function() {
 });
 
   //function to create markers of coordinates
-  function createMarkers(locationArr, customIcon){
+  function createMarkers(locationArr, someIcon){
     locationArr.forEach(function(location){
       //dig down into location (object) and get the data you need, in this case the Long and Lat
       var tempLat = location.geometry.coordinates[1];
       var tempLng = location.geometry.coordinates[0];
       var tempContentString = "<p>" + location.properties.title + "</p>";
+      //create info box that will appear on map with content from tempContentString
       var tempInfoWindow = new google.maps.InfoWindow({
         content: tempContentString
+      });
+      //create marker that will take lat-long of our data
+      var tempMarker =  new google.maps.Marker({
+        position: new google.maps.LatLng(tempLat, tempLng),
+        map: map,
+        title: location.properties.title,
+        icon: someIcon
+      });
+      //action on click of marker to open InfoWindow
+      tempMarker.addListener('click', function(){
+        tempInfoWindow.open(map, tempMarker);
       });
     });
   }
 
-
-
-
-
+  //handlebars function
+  function compileHandlebarsTemplate(data, targetHtml, targetScript){
+       var source = $(targetScript).html();
+       template = Handlebars.compile(source);
+       var dataTemplate = template({quakes: data});
+       $(targetHtml).append(dataTemplate);
+   }
 
 //function to create the map
 function initMap(){
